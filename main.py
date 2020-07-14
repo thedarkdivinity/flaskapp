9eb29e43778d0a3c23f0aac8690eb2aabddaca3c
Ddb123 <49611843+Ddb123@users.noreply.github.com>
2020-07-14 21:26:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.session import Session
+import math
 import json
 local_server = True
 
@@ -38,9 +39,30 @@ class Posts (db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:params['numberofposts']]
-    return render_template("index.html", params=params, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['numberofposts']))
+    #[0: params['no_of_posts']]
+    #posts = posts[]
+    page = request.args.get('page')
+    if(not str(page).isnumeric()):
+        page = 1
+    page= int(page)
+    posts = posts[(page-1)*int(params['numberofposts']): (page-1)*int(params['numberofposts'])+ int(params['numberofposts'])]
+    #Pagination Logic
+    #First
+    if (page==1):
+        prev = "#"
+        next = "/?page="+ str(page+1)
+    elif(page==last):
+        prev = "/?page=" + str(page - 1)
+        next = "#"
+    else:
+        prev = "/?page=" + str(page - 1)
+        next = "/?page=" + str(page + 1)
 
+
+
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 @app.route("/about")
 def about():
@@ -98,4 +120,4 @@ def edit(sno):
 
 
  
-app.run(debug=True,port=9000)
+app.run(debug=True,port=5500)
